902285e582aeb510076577fd40442661b504303f
Pasha <vfaryvo@mail.ru>
2018-12-22 13:13:37 +0500

    start

--- src/components/ChristmasTree/ChristmasTree.js
+++ src/components/ChristmasTree/ChristmasTree.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import styles from './style.css';
-/*
-class Fire extends React.Component {
-    render () {
-        return (
+import Lighter from "../Lighter/Lighter";
 
-        )
-    }
-}
-*/
 export default class ChristmasTree extends React.Component {
     render () {
         return (
             <div className={styles.tree}>
                 <div className={styles.base} />
                 <div className={styles.layer} />
-                <div className={`${styles.layer} ${styles.two}`} />
+                <div className={`${styles.layer} ${styles.two}`}>
+                    <Lighter/>
+                </div>
                 <div className={`${styles.layer} ${styles.three}`} />
                 <div className={`${styles.layer} ${styles.four}`} />
             </div>
--- src/components/Lighter/Lighter.js
+++ src/components/Lighter/Lighter.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import styles from './style.css';
 
-export default class Fire extends React.Component {
-    render () {
-        return (
-            <
-        );
-    }
+export default function Lighter() {
+    return (
+        <div className={styles.lighter}>
+        </div>
+    )
+
 }
 
-export default class ChristmasTree extends React.Component {
-    render () {
-        return (
-            <div className={styles.tree}>
-                <div className={styles.base} />
-                <div className={styles.layer} />
-                <div className={`${styles.layer} ${styles.two}`} />
-                <div className={`${styles.layer} ${styles.three}`} />
-                <div className={`${styles.layer} ${styles.four}`} />
-            </div>
-        );
-    }
-}
+
+
