8501c4de8ca1be044163148d2f8354301b9a0d43
Pasha <vfaryvo@mail.ru>
2018-12-22 16:11:14 +0500

    refactoring

--- src/components/ChristmasTree/ChristmasTree.js
+++ src/components/ChristmasTree/ChristmasTree.js
@@ -5,21 +5,23 @@ import Lighter from "../Lighter/Lighter";
 export default class ChristmasTree extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {ids : [], pointer : 0, rightLighters : [], colors : {}};
+        this.ids = [];
+        this.pointer = 0;
+        this.state = {colors : {}};
 
         this.lighterCount = props.number;
         for (let i = 1; i <= this.lighterCount; i++) {
-            this.state.ids.push(i);
+            this.ids.push(i);
             this.state.colors[i] = "grey";
         }
-        this.state.rightLighters = this.shuffleIds(this.state.ids);
+        this.rightLighters = this.shuffleIds(this.ids);
     }
 
     shuffleIds(ids) {
-        var result = [...ids];
-        for (var i = result.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = result[i];
+        let result = [...ids];
+        for (let i = result.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = result[i];
             result[i] = result[j];
             result[j] = temp;
         }
@@ -27,23 +29,29 @@ export default class ChristmasTree extends React.Component {
     }
 
     handleLighter (id) {
-        const self = this;
-        return function () {
-            alert(self.state.rightLighters);
-            alert(id);
-            if (self.state.rightLighters[self.state.pointer] === id) {
-                self.state.colors[id] = "yellow";
-                self.state.pointer++;
+        if (!id) return;
+        console.log(this.rightLighters)
+        return () => {
+            if (this.rightLighters[this.pointer] === id) {
+                const newState = {...this.state};
+                newState.colors[id] = 'yellow';
+                this.setState(newState);
+                this.pointer++;
             } else {
-                
-                for (let i = 1; i <= self.state.rightLighters.length; i++) {
-                    self.state.colors[i] = "grey";
+                const newState = {colors : {}};
+                for (let i = 1; i <= this.rightLighters.length; i++) {
+                    newState.colors[i] = 'grey'
                 }
-
-                self.state.pointer = 0;
+                this.setState(newState);
+                this.pointer = 0;
             }
-
-            self.setState(self.state);
+            if(this.pointer === this.rightLighters.length) {
+                const newState = {colors: {}}
+                for (let i = 1; i <= this.rightLighters.length; i++) {
+                    newState.colors[i] = 'grey'
+                }
+                setTimeout(() => alert('GZ'));
+            }
         }
     }
 
@@ -60,7 +68,7 @@ export default class ChristmasTree extends React.Component {
 
                 </div>
                 <div className={`${styles.layer} ${styles.four}`}>
-                    {this.state.ids.map(ide => (<Lighter color={this.state.colors[ide]} id={ide} key={ide} onClick = {this.handleLighter( ide)}/>))}
+                    {this.ids.map(ide => (<Lighter color={this.state.colors[ide]} id={ide} key={ide} onClick = {this.handleLighter( ide)}/>))}
                 </div>
             </div>
         );
